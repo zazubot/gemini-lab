a5e2d1aced7d506d175f29edb2b21306c6439623
ahmedkhaled4d <ahmedkhaled4d@gmail.com>
2024-07-15 13:19:53 +0300

    gemini-pro Lib

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { env } from "./utils/config";
+
+async function main() {
+  try {
+    console.log(`Typescript gemini-pro Lib : API KEY ${env.API_KEY}`);
+  } catch (e) {
+    console.error(e);
+  }
+}
+main();
